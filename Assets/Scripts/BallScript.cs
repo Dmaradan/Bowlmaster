546dfd33bedd732fb682833b1e41720e8956aea4
Dmaradan <diegomartin1313@gmail.com>
2016-10-01 23:53:56 -0700

    Add ball script with physics, velocity, and rolling audio

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+    public float velocity = 1;
+    Rigidbody rigidBody;
+    AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
+
+        rigidBody.velocity = Vector3.forward * velocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        source.Play();
+    }
+}
