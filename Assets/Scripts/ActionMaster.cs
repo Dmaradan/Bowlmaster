54628c0a2581071cd5b37791eff95402c81b0d36
Diego Martin <diegomartin1313@gmail.com>
2016-10-18 18:36:54 -0700

    Add more test coverage

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -5,14 +5,29 @@ public class ActionMaster {
 
 	public enum Action {Tidy, Reset, EndTurn, EndGame};
 
-	//private int[] bowls = new int[21];
+	private int[] bowls = new int[21];
 	private int bowl = 1;
 
 	public Action Bowl (int pins) {
 		
 		if(pins < 0 || pins > 10) {throw new UnityException("Invalid pin amount");}
 
-		//other behavior
+		//last frame
+		if(bowl == 19) {
+			bowls[bowl - 1] = pins;
+			bowl += 1;
+			return Action.Tidy;
+		} else if(bowl == 20) {
+			bowls[bowl - 1] = pins;
+			if(bowls[18] + bowls[19] == 10) {
+				//reset for last frame
+				bowl += 1;
+				return Action.Reset;
+			} else {
+				return Action.EndGame;
+			} 
+		}
+
 
 		if(pins == 10) {
 			bowl += 2;
@@ -30,5 +45,25 @@ public class ActionMaster {
 		//other behavior
 
 		throw new UnityException("Not sure what action to return!");
+	}
+
+	public void setBowl(int theBowl) {
+		bowl = theBowl;
+	}
+
+	public int getBowl() {
+		return bowl;
+	}
+
+	public int getSum() {
+		return bowls[18] + bowls[19];
+	}
+
+	public int readNineteen() {
+		return bowls[18];
+	}
+
+	public int readTwenty() {
+		return bowls[19];
 	}
 }
--- Assets/Scripts/Editor/ActionMasterTest.cs
+++ Assets/Scripts/Editor/ActionMasterTest.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 public class ActionMasterTest {
 
 	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+	private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
 	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
 	private ActionMaster.Action reset = ActionMaster.Action.Reset;
 
@@ -29,15 +30,35 @@ public class ActionMasterTest {
 		Assert.AreEqual(tidy, actionMaster.Bowl(8));
 	}
 
-	// [Test]
-	// public void T03TwoBowlsReturnReset () {
-	// 	actionMaster.Bowl(2);
-	// 	Assert.AreEqual(reset, actionMaster.Bowl(3));
-	// }
+	[Test]
+	public void T031AndSpareLastFrameReturnReset () {
+		actionMaster.setBowl(19);
+		actionMaster.Bowl(1);
+		Assert.AreEqual(reset, actionMaster.Bowl(9));
+	}
 
 	[Test]
 	public void T0428ReturnsEndTurn() {
 		actionMaster.Bowl(2);
 		Assert.AreEqual(endTurn, actionMaster.Bowl(8));
+	}
+
+	[Test]
+	public void T05SetBowlActuallyWorks() {
+		actionMaster.setBowl(19);
+		Assert.AreEqual(19, actionMaster.getBowl());
+	}
+
+	[Test]
+	public void T0619thBowlReturnsTidy() {
+		actionMaster.setBowl(19);
+		Assert.AreEqual(tidy, actionMaster.Bowl(1));
+	}
+
+	[Test]
+	public void T07NoSpareInLastFrameReturnsEndGame() {
+		actionMaster.setBowl(19);
+		actionMaster.Bowl(3);
+		Assert.AreEqual(endGame, actionMaster.Bowl(2));
 	}
 }
